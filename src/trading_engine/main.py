0acf77de5b1d868a2ab6947d97eb8845d6c33e80
Eko Widianto <ekowidianto@outlook.com>
2024-05-28 19:28:08 +0800

    chore: run formatter

--- src/trading_engine/main.py
+++ src/trading_engine/main.py
@@ -24,7 +24,9 @@ class TradingStrategy:
         self.peaks = []
         self.troughs = []
         self.smoothed_prices = []
-        self.model = joblib.load("training_engine/outputs/logistic_regression_model.pkl")
+        self.model = joblib.load(
+            "training_engine/outputs/logistic_regression_model.pkl"
+        )
 
     def collect_new_data(self):
         new_rows = []
