cb48208373ff0a603f90ce30df20dd0452321f53
Eko Widianto <ekowidianto@outlook.com>
2024-06-04 19:20:55 +0800

    bookkeeper wip

--- src/book_keeper/main.py
+++ src/book_keeper/main.py
@@ -1,77 +1,152 @@
+import pandas as pd
+
+
 class BookKeeper:
     """
-    - stores all open trades and executed trades as well as technical indicator values
-      surrounding trade at the time of execution
-    - will interact with ML modeling in ReviewEngine class for review to assess effectiveness
-      and for parameter tuning where applicable
-    - will interact with UI LivePlotter for user to monitor MTM / PnL on demand
-    - provides functionality for reporting and analysis
+    This class stores all executed trades and provides analyses based on executed trades.
     """
 
-    def __init__(self, ticker):
-        self.ticker = ticker
-        self.current_unrealized_pnl = 0
-        self.current_realized_pnl = 0
-        self.historical_trades = {}  # Stores trades indexed by trade ID
+    def __init__(self, initial_cash):
+        self.initial_cash = initial_cash
+        self.trades = pd.DataFrame(
+            columns=[
+                "TradeID",
+                "Date",
+                "Symbol",
+                "Quantity",
+                "Price",
+                "Type",
+                "TransactionCost",
+            ]
+        )
+        self.market_prices = pd.DataFrame(columns=["Date", "Symbol", "Price"])
+        self.realized_pnl = 0
+        self.unrealized_pnl = 0
+        self.positions = {}
 
     @property
-    def unrealized_pnl(self):
-        return self.current_unrealized_pnl
+    def get_initial_cash(self):
+        return self.initial_cash
 
     @property
-    def realized_pnl(self):
-        return self.current_realized_pnl
+    def get_unrealized_pnl(self):
+        return self.unrealized_pnl
 
-    def add_trade(self, trade):
-        """
-        Stores a new trade in the trades dictionary.
-        Each trade can be a dictionary with details like trade_id, status, instrument, volume, execution_price, etc.
-        """
-        # TODO: Implement the method to add a trade to the trades dict
-        self.__update_pnl()
-        pass
+    @property
+    def get_realized_pnl(self):
+        return self.realized_pnl
 
-    def get_trade(self, trade_id):
-        """
-        Returns details of a single trade by trade ID.
-        """
-        # TODO: Implement the method to retrieve a single trade's details
-        pass
+    @property
+    def get_trades(self):
+        return self.trades
 
-    def list_all_trades(self, filter_by=None):
-        """
-        Returns a list of all trades, optionally filtered by certain criteria.
-        """
-        # TODO: Implement the method to list trades with optional filtering
-        pass
+    @property
+    def get_trade_by_id(self, trade_id):
+        return self.trades[self.trades["TradeID"] == trade_id]
 
-    def calculate_pnl(self):
-        """
-        Calculate the profit and loss across all or filtered trades.
-        """
-        # TODO: Implement the method to calculate P&L
-        pass
+    def add_trade(
+        self, trade_id, date, symbol, quantity, price, trade_type, transaction_cost
+    ):
+        self.trades = self.trades.append(
+            {
+                "TradeID": trade_id,
+                "Date": date,
+                "Symbol": symbol,
+                "Quantity": quantity,
+                "Price": price,
+                "Type": trade_type,
+                "TransactionCost": transaction_cost,
+            },
+            ignore_index=True,
+        )
+        self._update_realized_pnl(symbol, quantity, price, trade_type, transaction_cost)
+        self._update_unrealized_pnl()
 
-    def generate_report(self, report_type):
-        """
-        Generate various types of reports (e.g., performance, compliance, risk management).
-        """
-        # TODO: Implement the method to generate specific types of reports
-        pass
+    def update_market_price(self, date, symbol, price):
+        self.market_prices = self.market_prices.append(
+            {"Date": date, "Symbol": symbol, "Price": price}, ignore_index=True
+        )
+        self._update_unrealized_pnl()
 
-    def export_data(self, format_type="csv"):
+    def export_data(self):
         """
-        Export the trades and indicators to a file in a specified format, such as CSV or JSON.
+        Export the trades in CSV format.
         """
-        # TODO: Implement data export functionality
-        pass
+        self.trades.to_csv("outputs/trades.csv")
 
-    def synchronize_with_risk_manager(self, risk_manager):
-        """
-        Synchronize with the RiskManager to update trading limits and risk metrics.
-        """
-        # TODO: Implement synchronization with RiskManager
-        pass
+    def calculate_max_drawdown(self):
+        pnl_series = self._calculate_pnl_series()
+        roll_max = pnl_series.cummax()
+        drawdown = (pnl_series - roll_max) / roll_max
+        return drawdown.min()
 
-    def __update_pnl(self):
+    def calculate_sharpe_ratio(self, risk_free_rate=0):
+        pnl_series = self._calculate_pnl_series()
+        returns = pnl_series.pct_change().dropna()
+        excess_returns = returns - risk_free_rate
+        return excess_returns.mean() / excess_returns.std()
+
+    def _calculate_pnl_series(self):
+        portfolio_value = self.trades.apply(
+            lambda row: (row["Price"] * row["Quantity"])
+            if row["Type"] == "buy"
+            else (-row["Price"] * row["Quantity"]),
+            axis=1,
+        )
+        pnl_series = portfolio_value.cumsum()
+        return pnl_series
+
+    def _update_realized_pnl(
+        self, symbol, quantity, price, trade_type, transaction_cost
+    ):
+        if symbol not in self.positions:
+            self.positions[symbol] = {"cost": 0, "quantity": 0}
+
+        curr_cost, curr_quantity = (
+            self.positions[symbol]["cost"],
+            self.positions[symbol]["quantity"],
+        )
+
+        if trade_type == "buy":
+            if curr_quantity < 0:
+                # Selling short positions
+                quantity_to_close = min(abs(curr_quantity), quantity)
+                trade_pnl = quantity_to_close * (curr_cost - price)
+                self.realized_pnl += trade_pnl
+
+            # Adding to long position
+            new_quantity = curr_quantity + quantity
+            new_cost = (curr_quantity * curr_cost + quantity * price) / new_quantity
+            self.positions[symbol] = {"quantity": new_quantity, "cost": new_cost}
+        # elif trade_type == "sell":
+        #     if curr_quantity > 0:
+        #         # Selling long positions
+        #         quantity_to_close = min((curr_quantity), quantity)
+        #         trade_pnl = quantity_to_close * (price - curr_price)
+        #         self.realized_pnl += trade_pnl
+        #         self.positions[symbol] -= quantity_to_close
+
+        #     # Adding to short position
+        #     self.positions[symbol] -= quantity
+
+        #     if positions[asset]["quantity"] > 0:
+        #         # Closing long positions
+        #         quantity_to_close = min(positions[asset]["quantity"], quantity)
+        #         realized_pnl += quantity_to_close * (price - positions[asset]["price"])
+        #         positions[asset]["quantity"] -= quantity_to_close
+
+        #     # Adding to short position
+        #     new_quantity = positions[asset]["quantity"] - quantity
+        #     new_price = (
+        #         (
+        #             abs(positions[asset]["quantity"]) * positions[asset]["price"]
+        #             + quantity * price
+        #         )
+        #         / abs(new_quantity)
+        #         if new_quantity != 0
+        #         else 0
+        #     )
+        #     positions[asset] = {"quantity": new_quantity, "price": new_price}
+
+    def _update_unrealized_pnl(self):
         pass
